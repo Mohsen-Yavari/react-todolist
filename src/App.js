4a802af3c8498e0c3d39a990bc325b38315e7770
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-05-10 15:51:21 +0330

    create add task our delete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import React,{useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
 
+  // Todolist State
+  const [toDo,setTodo] = useState([
+    {"id":1,"title":" کار1","status":false},
+    {"id":1,"title":" کار2","status":false},
+  ]); 
+
+  const [newTask,setNewTask] = useState("");
+  const [updateDate,setUpdateDate] = useState("");
+
+  //Add task
+  const 
+
+  return (
+    <div className="container App">
+        <br /><br />
+        <h2>لیست انجام کارها</h2>
+        <br /><br />
+        
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'remixicon/fonts/remixicon.css';
+
+//font
+import "./font/B-NAZANIN.TTF";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
